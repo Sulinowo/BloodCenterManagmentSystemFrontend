a0864824fe0bda5445ff47bfbfbffee9e0ad8e20
Sulinowo <kamilkalwejt@gmail.com>
2022-01-11 21:48:56 +0100

    Delete trips from informations

--- bcmsfront/src/app/shared/sites/informations/components/trips/trips.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-trips',
-  templateUrl: './trips.component.html',
-  styleUrls: ['./trips.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class TripsComponent {}
--- bcmsfront/src/app/shared/sites/informations/informations.module.ts
+++ bcmsfront/src/app/shared/sites/informations/informations.module.ts
@@ -5,14 +5,12 @@ import { InformationsRoutingModule } from './informations-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RegistrationHoursComponent } from './components/registration-hours/registration-hours.component';
 import { CovidInformationComponent } from './components/covid-information/covid-information.component';
-import { TripsComponent } from './components/trips/trips.component';
 
 @NgModule({
   declarations: [
     InformationsPageComponent,
     RegistrationHoursComponent,
     CovidInformationComponent,
-    TripsComponent,
   ],
   imports: [CommonModule, InformationsRoutingModule, FlexLayoutModule],
 })
